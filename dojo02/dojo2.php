693fc7d07340325a38312f79c82610862f793dbd
Dieison <dieisoncomix@gmail.com>
2018-09-04 13:44:29 -0300

    Piloto: vieira CoPiloto: rivelino

--- dojo02/dojo2.php
+++ dojo02/dojo2.php
@@ -2,3 +2,18 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body> 
+	<form >
+		<label>Informe um numero:</label>
+		<input type="text" name="numero" value="">
+		<input type="submit" value="calcular">
+	</form>
+
+</body>
+</html>
